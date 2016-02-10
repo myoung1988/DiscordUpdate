3e0c6b3f105f1fb42f5f3732acd1cb2c4c2ad5eb
Michael Young <mike1988@prodigy.net>
2016-02-10 02:11:07 -0600

    Initial

---
+++ DiscordUpdate/Main.vb
@@ -0,0 +1,10 @@
+﻿Public Class Main
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Text = TextBox1.Text
+        MessageBox.Show(Me, "Discord", "Once running, add it under User Settings > Game", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Text = Me.Text
+    End Sub
+End Class
